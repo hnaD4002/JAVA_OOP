dec80d9a4c5b67fec97fc2b48b77c70cd9e65b93
DanhNe <trongdanh2004qlql@gmail.com>
2023-08-25 21:59:31 +0700

    Ho Trong Danh

---
+++ Buoi_5/BTVN/Bai_2/School.java
@@ -0,0 +1,34 @@
+package BTVN.Bai_2;
+
+public class School {
+	private String name, date;
+
+	public School() {
+	}
+
+	public School(String name, String date)
+	{
+		this.name = name;
+		this.date = date;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+	public void setDate(String date)
+	{
+		this.date = date;
+	}
+
+	public String getDate()
+	{
+		return this.date;
+	}
+
+}
---
+++ Buoi_5/Online/src/Point.java
@@ -0,0 +1,29 @@
+package Online.src;
+
+class Point {
+	private int x;
+	private int y;
+
+	public Point(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+
+	public int getX() {
+		return this.x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getY() {
+		return this.y;
+	}
+}
